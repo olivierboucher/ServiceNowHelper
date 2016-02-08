69bf8675a8fed2dbe37c853e0d9e3598c2c810e0
Olivier Boucher <boucher.oli@gmail.com>
2016-02-08 17:17:37 -0500

    hello world

---
+++ main.js
@@ -0,0 +1,28 @@
+import Electron from 'electron'
+
+const Application = Electron.app;
+const BrowserWindow = Electron.BrowserWindow;
+
+let mainWindow;
+
+function createMainWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL('file://' + __dirname + '/../index.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    });
+}
+
+Application.on('ready', createMainWindow);
+
+Application.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        Application.quit();
+    }
+});
+
+Application.on('activate', () => {
+    if(mainWindow === null){
+        createMainWindow();
+    }
+});
