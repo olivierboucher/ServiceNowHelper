3293bbdeb6a1e587ee00747f2d884346ee5be2a0
Olivier Boucher <boucher.oli@gmail.com>
2016-02-08 20:44:12 -0500

    registered the service

--- app/src/app.js
+++ app/src/app.js
@@ -2,9 +2,11 @@ import Angular from 'angular';
 import Router from 'angular-route';
 import Config from './config'
 import LoginController from './controllers/login.controller';
+import NowService from './services/now.service';
 
 var app = Angular.module('app', [Router])
     .config(Config)
-    .controller('LoginController', LoginController);
+    .controller('LoginController', LoginController)
+    .service('NowService', NowService.NowFactory);
 
 
