e446d37aa66dfc74b126657c0f56f86cf6d0ea5e
Olivier Boucher <boucher.oli@gmail.com>
2016-02-08 19:20:14 -0500

    added devtools

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ let mainWindow;
 function createMainWindow() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadURL('file://' + __dirname + '/../index.html');
+    mainWindow.webContents.openDevTools();
     mainWindow.on('closed', () => {
         mainWindow = null
     });
