38a4758a6a81e10914ce879d1cf85f540e431763
Olivier Boucher <boucher.oli@gmail.com>
2016-02-08 21:39:40 -0500

    live reload

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var electron = require('electron-connect').server.create();
 
-gulp.task("default", function () {
-  return gulp.src("main.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+gulp.task('serve', function () {
+  // Start browser process
+  electron.start();
+
+  // Restart browser process
+  gulp.watch('dist/main.js', electron.restart);
+
+  // Reload renderer process
+  gulp.watch('app/dist/bundle.js', electron.reload);
 });
--- main.js
+++ main.js
@@ -1,28 +1,35 @@
-import Electron from 'electron'
+//import Electron from 'electron'
+//import { client as Client } from 'electron-connect'
+
+var Electron = require('electron');
+var Client = require('electron-connect').client;
 
 const Application = Electron.app;
 const BrowserWindow = Electron.BrowserWindow;
 
-let mainWindow;
+var mainWindow;
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({width: 800, height: 600});
-    mainWindow.loadURL('file://' + __dirname + '/../index.html');
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
     mainWindow.webContents.openDevTools();
-    mainWindow.on('closed', () => {
+    mainWindow.on('closed', function() {
         mainWindow = null
     });
 }
 
-Application.on('ready', createMainWindow);
+Application.on('ready', function() {
+    createMainWindow();
+    Client.create(mainWindow);
+});
 
-Application.on('window-all-closed', () => {
+Application.on('window-all-closed', function(){
     if (process.platform !== 'darwin') {
         Application.quit();
     }
 });
 
-Application.on('activate', () => {
+Application.on('activate', function(){
     if(mainWindow === null){
         createMainWindow();
     }
